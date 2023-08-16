8497cceeaf0c41452509ba8e427da1c98662a9ed
Luanna <lua.varjao@gmail.com>
2023-08-16 17:48:53 -0300

    Resolução Ex 01

---
+++ 01/scripts.js
@@ -0,0 +1,9 @@
+const frutas = ['Banana', 'Maçã', 'Abacaxi', 'Pêra', 'Uva'];
+
+frutas.reverse();
+console.log(frutas.join(", "))
+
+frutas.reverse()
+;frutas.splice(0,1);
+frutas.splice(-1,1,"Melão");
+console.log(frutas);
