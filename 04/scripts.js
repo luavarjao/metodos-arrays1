43b1b940693fdc5b11082e62f3ec0d8ca0a4f39c
Luanna <lua.varjao@gmail.com>
2023-08-17 09:36:51 -0300

    Resolução Ex 04

--- 04/scripts.js
+++ 04/scripts.js
@@ -1,1 +1,16 @@
 const pacientes = ['José', 'Pedro', 'Maria', 'João', 'Ana', 'Bárbara', 'Joana'];
+let operacao = "atender";
+
+function atendimento (pacientes, operacao){
+    if(operacao === "agendar"){
+        pacientes.push(pacientes[0]);
+        pacientes.shift();
+    } if (operacao === "atender"){
+        pacientes.shift();
+    } else {
+        console.log("Código inválido!")
+    }
+console.log(pacientes.join(", "));
+}
+
+atendimento(pacientes, operacao);
