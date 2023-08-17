c53a2f802e3a1ca48f0f1953767c5bd039644278
Luanna <lua.varjao@gmail.com>
2023-08-17 09:51:18 -0300

    Resolução Ex 05

--- 05/scripts.js
+++ 05/scripts.js
@@ -1,1 +1,35 @@
 const pacientes = ['José', 'Pedro', 'Maria', 'João', 'Ana', 'Bárbara', 'Joana'];
+let paciente = "";
+
+//a) Crie uma função `agendarPaciente` que será exclusiva para agendamentos. 
+//Ela deverá receber como argumento da função o array de pacientes e o paciente a 
+//ser agendado. A função deverá adicionar ao final da fila o paciente informado como 
+//argumento.
+
+function agendarPaciente (pacientes, paciente){
+    pacientes.splice(pacientes.indexOf(paciente), 1);
+    pacientes.push(paciente);
+    console.log(pacientes.join(", "));
+}
+
+//b) Crie uma função `atenderPaciente` que será exclusiva para atendimentos. 
+//Ela deverá receber o array de pacientes e sua função é remover o paciente atendido 
+//da fila. Os pacientes são atendidos por ordem na fila.
+
+function atenderPaciente (pacientes, paciente){
+    pacientes.splice(pacientes.indexOf(paciente), 1);
+    console.log(pacientes.join(", "));
+}
+
+//c) Crie uma função `cancelarAtendimento` que receberá o array de pacientes e o 
+//paciente que deseja cancelar o antendimento. A função deverá remover o paciente que 
+//deseja cancelar da fila de atendimento.
+
+function cancelarAtendimento (pacientes, paciente){
+    pacientes.splice(pacientes.indexOf(paciente), 1);
+    console.log(pacientes.join(", "));
+}
+
+agendarPaciente(pacientes, "Ana");
+atenderPaciente(pacientes, "Pedro");
+cancelarAtendimento(pacientes, "Maria");
